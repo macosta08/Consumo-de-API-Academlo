5b63c983fc96393a33309c455dded64773068765
Mariangel <mariangelacosta.88@gmail.com>
2020-10-13 00:11:56 -0500

    Filtros de cantidad de preguntas tipo y dificultad

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-// En este archivo importaremos los modulos
+
+function getQuestions() {
+    const questionsQuantity = document.getElementById('questions-number').value
+    const questionsType = document.getElementById('questions-type').value
+    const questionsDifficulty = document.getElementById('questions-difficulty').value
+    
+    fetch(`https://opentdb.com/api.php?amount=${questionsQuantity}&category=&difficulty=${questionsDifficulty}&type=${questionsType}`)
+        .then(response => response.json())
+        .then(data => printCards(data.results))
+}
+
+// console.log(datos);
+
+function printCards(questions) {
+    const container = document.getElementById('container-cards');
+    container.innerHTML = '';
+    questions.forEach(question => {
+        const card = returnCardHTML(question);
+        container.innerHTML += card;
+    });
+    // poner las preguntas en mi página web
+}
+
+function returnCardHTML(q) {
+    const card = `<div class="card">
+                    <div class="card-body">
+                    <h5 class="card-title">${q.category} - ${q.difficulty}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
+                        ${returnAnswersHTML(q.correct_answer, q.incorrect_answers)}           
+                    </div>
+                </div>`
+    return card;
+}
+
+
+function returnAnswersHTML(correct, incorrects) {
+    const correctHTML = `<div class="form-check">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                            <label class="form-check-label" for="exampleRadios1">
+                            ${correct}
+                            </label>
+                        </div>`;
+
+
+    let incorrectHTML = '';
+    incorrects.forEach((incorrect) => {
+        incorrectHTML += `<div class="form-check">
+                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                            <label class="form-check-label" for="exampleRadios1">
+                            ${incorrect}
+                            </label>
+                        </div>`;
+    })
+
+
+    return correctHTML + incorrectHTML;
+}
